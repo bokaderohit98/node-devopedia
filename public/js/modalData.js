d7d5792ec738ee367c304127b5eb2d29f53cf77e
GaganBhayana <gagan2oct1998@gmail.com>
2019-04-04 12:57:47 +0530

    Added material.js files

---
+++ public/js/modalData.js
@@ -0,0 +1,6 @@
+$('#editRequestModal').on('show.bs.modal', function(event) {
+  var button = $(event.relatedTarget)
+  var id = button.data('id')
+  var modal = $(this)
+  modal.find('#id').val(id)
+});
